51e3d2c85f77b95a56b8413b001426fc0dfe4be4
Tej Pratap <bankebihari5463@gmail.com>
2023-06-19 01:52:51 +0530

    making api requests

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 
 class Loading extends StatefulWidget {
@@ -9,6 +11,32 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  @override
+  void initState() {
+    super.initState();
+    print("init state of choose location");
+    getData();
+  }
+
+  void getData () async{
+    print("get data runs");
+    //const String apiEndpoint = 'https://tejtech.onrender.com/';
+    const String apiEndpoint = 'https://jsonplaceholder.typicode.com/todos/1';
+
+    final Uri url = Uri.parse(apiEndpoint);
+    var response = await http.get(url);
+    print(response.body);
+    Map data = jsonDecode(response.body);
+    print(data);
+    print(data['userId']);
+    print(data['title']);
+    print(data['completed']);
+
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
